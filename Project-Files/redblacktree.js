27ef4a1198bd226e5b0b6fb9fa7e2512c56c0d2a
Aren <aren.tahmasian.745@my.csun.edu>
2017-10-14 16:23:41 -0700

    Fixed jQuery loading issue and added insert/delete

--- Project-Files/redblacktree.js
+++ Project-Files/redblacktree.js
@@ -2,15 +2,37 @@
  * Created by Aren on 10/14/2017.
  */
 
-console.log("Hello World!");
 
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-};
+// Testing if jQuery is functioning properly
+// window.onload = function() {
+//     if (window.jQuery) {
+//         // jQuery is loaded
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// };
 
+$(document).ready(function(){
+
+
+    $('#insert').on('input', function(){ //insert function
+        let text = $('#insert').val();
+        let display = $('#output');
+        text = parseInt(text, 10); //remove leading zeros
+        if(isNaN(text)){ //check if input is not a number
+            display.text(0);
+        }
+        else{
+            display.text(text);
+
+        }
+    });
+
+});
+
+
+// window.onload = function () {
+//
+// };
